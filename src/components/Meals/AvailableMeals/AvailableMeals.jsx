e8016f91207740476df0f7e2088e65ad18934e7e
TarDasT-7 <amirtardast7shiraz@gmail.com>
2022-08-24 23:57:36 +0430

    items and ...

--- src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -1,40 +1,44 @@
 import React from 'react';
 import styles from './AvailableMeals.module.scss';
 
+import Card from '../../UI/card/Card';
+import MealItem from '../MealItems/MealItem';
+import MealItemForm from '../MealItemForm/MealItemForm';
+
 const DUMMY_MEALS =[
 
     {
         id: 'b1',
         name: 'Slim Burger',
-        desciption : 'Mac Bread, Beef(150 grams), Lettuce, Tomato',
+        description : 'Mac Bread, Beef(150 grams), Lettuce, Tomato',
         price : 2.95
     },
 
     {
         id: 'b2',
         name: 'Super Burger',
-        desciption : 'Mac Bread, Two Beefs(250 grams), Grilled Chicken(80 grams), Smoked Sauce, Lettuce, Tomato',
+        description : 'Mac Bread, Two Beefs(250 grams), Grilled Chicken(80 grams), Smoked Sauce, Lettuce, Tomato',
         price : 9.95
     },
 
     {
         id: 'b3',
         name: 'Cheese Burger',
-        desciption : 'Mac Bread, Beef(250 grams), Cheese(gouda), Smoked Sauce, Lettuce, Tomato',
+        description : 'Mac Bread, Beef(250 grams), Cheese(gouda), Smoked Sauce, Lettuce, Tomato',
         price : 6.55
     },
 
     {
         id: 'b4',
         name: 'Mushroom Burger',
-        desciption : 'Mac Bread, Beef(250 grams), Mushroom, Smoked Sauce, Mushroom Sauce, Lettuce, Tomato',
+        description : 'Mac Bread, Beef(250 grams), Mushroom, Smoked Sauce, Mushroom Sauce, Lettuce, Tomato',
         price : 6.25
     },
 
     {
         id: 'b5',
         name: 'Bacon Burger BBQ',
-        desciption : 'Mac Bread, Grilled Beef(300 grams), Bacon(80 grams), Smoked Sauce, Garlic Sauce, Tomato',
+        description : 'Mac Bread, Grilled Beef(300 grams), Bacon(80 grams), Smoked Sauce, Garlic Sauce, Tomato',
         price : 8.45
     },
 
@@ -44,15 +48,18 @@ const AvailableMeals = props => {
 
     const mealsList = DUMMY_MEALS.map( burger => (
 
-        <li key={burger.id} >{burger.name}</li>
+        <MealItem key={burger.id} name={burger.name} description={burger.description} price={burger.price} />
 
     ) );
 
     return(
         <section className={styles.meals}>
-            <ul>
-                {mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
+            
         </section>
     );
 
